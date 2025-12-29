1e5a35f269d5722973025922070e04e2bb3e3943
harshmalu9 <hmalu975@gmail.com>
2025-12-29 14:33:20 +0530

    learnt to use tailwind in react and also created a dummy standard  header

---
+++ TrailwindTesting/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-20 bg-green-100 flex justify-between p-4'>
+      <h1 className='text-4xl bg-amber-400'>LOGO</h1>
+      <div className='flex justify-center space-x-2'>
+        <h1 className='text-1xl bg-red-300'>This is the header component</h1>
+        <h1 className='text-1xl bg-red-300'>This is the header component</h1>
+        <h1 className='text-1xl bg-red-300'>This is the header component</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
